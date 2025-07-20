1627b7fe163ad8f9f87d8dfddd8d602113872773
Samin Rahman Khan <saminrahmankhan97@gmail.com>
2025-07-20 12:18:15 +0600

    Add preprocessor for Calcagno dataset

---
+++ preprocessors/preprocess_calcagno.py
@@ -0,0 +1,177 @@
+ROOT_PATH = '/content/drive/MyDrive/Research - Sohel sir/Pathological Cells/Pathological Cell Identification/data/Infracted Heart Border Zone - GSE214611_RAW'
+
+import scanpy as sc
+import pandas as pd
+import anndata as AD
+
+
+metadata_sc = pd.read_csv(f'{ROOT_PATH}/sn_wc_object_integrated@meta.data.csv', index_col=0)
+metadata_sc_t = metadata_sc.rename(index=lambda x: x.split('_')[0])
+metadata_sc_t['obs_names'] = metadata_sc_t.index
+
+def load_raw_anndata(mtx_path_id, metadata_orig_id):
+    PATH = f'{ROOT_PATH}/{mtx_path_id}'
+    ad = sc.read_10x_mtx(PATH)
+    ad.obs = ad.obs.rename(index=lambda x: x.split('-')[0])
+    A = metadata_sc_t[metadata_sc_t['orig.ident'] == metadata_orig_id]
+
+    ad.obs = ad.obs.merge(A, left_index=True, right_index=True, how='left')
+
+    return ad
+
+def preprocess_anndata(ad, n_top_genes=None, plot=False):
+    ad.var_names_make_unique()
+
+    sc.pp.filter_cells(ad, min_genes=200)
+    sc.pp.filter_genes(ad, min_cells=3)
+
+    ad.var['mt'] = ad.var_names.str.startswith('mt-')
+    sc.pp.calculate_qc_metrics(ad, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+    if plot:
+        sc.pl.violin(ad, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
+                jitter=0.4, multi_panel=True)
+        sc.pl.scatter(ad, x='total_counts', y='pct_counts_mt')
+        sc.pl.scatter(ad, x='total_counts', y='n_genes_by_counts')
+
+    ad = ad[ad.obs.pct_counts_mt <= 5, :]
+
+    ad.layers['counts'] = ad.X.copy()
+
+    sc.pp.normalize_total(ad, target_sum=1e4)
+    sc.pp.log1p(ad)
+
+    if n_top_genes is not None:
+        sc.pp.highly_variable_genes(ad, n_top_genes=n_top_genes)
+        ad.raw = ad
+        ad = ad[:, ad.var.highly_variable]
+
+    if plot:
+        sc.pl.highly_variable_genes(ad)
+
+    return ad
+
+
+def get_mtx_path_id_metadata_orig_id(problem_key):
+    exp_meta_keys = {
+        'ref_0_que_1hr': {
+            'reference': [
+                ('snd0_1', 'D0'),
+                ('snd0_2', 'D0'),
+                ('snd0_3', 'NoMI_Nuclei'),
+            ],
+            'query': [
+                ('sn1hr_1', 'sn1_1'),
+                ('sn1hr_2', 'sn1_2'),
+            ]
+        },
+        'ref_0_que_4hr': {
+            'reference': [
+                ('snd0_1', 'D0'),
+                ('snd0_2', 'D0'),
+                ('snd0_3', 'NoMI_Nuclei'),
+            ],
+            'query': [
+                ('sn4hr', '4HR'),
+            ]
+        },
+        'ref_0_que_1': {
+            'reference': [
+                ('snd0_1', 'D0'),
+                ('snd0_2', 'D0'),
+                ('snd0_3', 'NoMI_Nuclei'),
+            ],
+            'query': [
+                ('snd1_1', 'D1_MI'),
+                ('snd1_2', 'D1_MI_Hrt_nuclei'),
+                ('snd1_3', 'D1_IR30_Hrt_nuclei'),
+                ('snd1_4', 'WT_IR30_D1_2'),
+                ('snd1_5', 'WT_IR30_D1_3'),
+            ]
+        },
+        'ref_0_que_3': {
+            'reference': [
+                ('snd0_1', 'D0'),
+                ('snd0_2', 'D0'),
+                ('snd0_3', 'NoMI_Nuclei'),
+            ],
+            'query': [
+                ('snd3_1', 'WT_IR30_D3'),
+                ('snd3_2', 'D3F_M_rep1'),
+                ('snd3_3', 'D3F_M_rep2'),
+            ]
+        },
+        'ref_0_que_7': {
+            'reference': [
+                ('snd0_1', 'D0'),
+                ('snd0_2', 'D0'),
+                ('snd0_3', 'NoMI_Nuclei'),
+            ],
+            'query': [
+                ('snd7_1', 'snD7_1'),
+                ('snd7_2', 'snD7_1'),
+                ('snd7_3', 'D5_D7'),
+            ]
+        },
+        'ref_0_que_1-7': {
+            'reference': [
+                ('snd0_1', 'D0'),
+                ('snd0_2', 'D0'),
+                ('snd0_3', 'NoMI_Nuclei'),
+            ],
+            'query': [
+                ('sn1hr_1', 'sn1_1'),
+                ('sn1hr_2', 'sn1_2'),
+                ('sn4hr', '4HR'),
+                ('snd1_1', 'D1_MI'),
+                ('snd1_2', 'D1_MI_Hrt_nuclei'),
+                ('snd1_3', 'D1_IR30_Hrt_nuclei'),
+                ('snd1_4', 'WT_IR30_D1_2'),
+                ('snd1_5', 'WT_IR30_D1_3'),
+                ('snd3_1', 'WT_IR30_D3'),
+                ('snd3_2', 'D3F_M_rep1'),
+                ('snd3_3', 'D3F_M_rep2'),
+                ('snd7_1', 'snD7_1'),
+                ('snd7_2', 'snD7_1'),
+                ('snd7_3', 'D5_D7'),
+            ]
+        }
+    }
+
+    return exp_meta_keys[problem_key]
+
+
+imp_celltypes = ['Ankrd1', 'Xirp2', 'Myh6']
+
+
+def get_preprocessed_anndata(PROBLEM_KEY, n_top_genes=5_000):
+
+    exp_meta_keys = get_mtx_path_id_metadata_orig_id(PROBLEM_KEY)
+
+    adatas = {}
+
+    for key, item in exp_meta_keys.items():
+        for i, (mtx_path_id, metadata_orig_id) in enumerate(item):
+            ad = preprocess_anndata(load_raw_anndata(mtx_path_id, metadata_orig_id))
+            ad = ad[ad.obs['final_cluster'].isin(imp_celltypes)]
+            dataset_id = f"{key}_{i+1}"
+            ad.obs['sample'] = dataset_id
+            ad.obs['dataset'] = key
+            adatas[f"{key}_{i+1}"] = ad
+
+    ad = AD.concat(adatas, label='batch_key')
+    ad.obs_names_make_unique()
+
+    cluster_to_zone = {
+        "Ankrd1": "BZ1",
+        "Xirp2": "BZ2",
+        "Myh6": "RZ",
+    }
+
+    ad.obs["zone"] = ad.obs["final_cluster"].map(cluster_to_zone)
+
+    if n_top_genes is not None and n_top_genes < ad.shape[1]:
+        sc.pp.highly_variable_genes(ad, n_top_genes=n_top_genes)
+        ad.raw = ad
+        ad = ad[:, ad.var.highly_variable]
+
+    return ad
