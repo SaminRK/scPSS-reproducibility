faf01a7d2936a1e3f279e3683e2362ce73a803fa
Samin Rahman Khan <saminrahmankhan97@gmail.com>
2025-07-20 12:34:05 +0600

    Fix

--- data_downloaders/download_calcagno.sh
+++ data_downloaders/download_calcagno.sh
@@ -12,6 +12,17 @@ DATA_DIR="$1"
 # Create directory
 mkdir -p "$DATA_DIR"
 
+METADATA_URL="https://zenodo.org/records/7055957/files/Nikatag/Single-Cell-Spatial-Transcriptomics-for-Border-zone-BZ_Cell_Mapping.zip?download=1"
+ZIP_OUTPUT="$DATA_DIR/Single-Cell-Spatial-Transcriptomics-for-Border-zone-BZ_Cell_Mapping_zenodo.zip"
+
+echo "==> Downloading Zenodo ZIP..."
+wget -c "$METADATA_URL" -O "$ZIP_OUTPUT"
+
+echo "==> Unzipping Zenodo ZIP..."
+unzip -o "$ZIP_OUTPUT" -d "$DATA_DIR"
+
+mv "$DATA_DIR"/Nikatag-Single-Cell-Spatial-Transcriptomics-for-Border-zone-*/* "$DATA_DIR"/
+
 echo "==> Downloading GEO dataset..."
 wget -c "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE214611&format=file" \
      -O "$DATA_DIR/GSE214611_RAW.tar"
@@ -24,17 +35,6 @@ cd "$DATA_DIR"
 for f in *.tar.gz; do
     tar -xvzf "$f" -C "$(dirname "$f")"
 done
-
-METADATA_URL="https://zenodo.org/records/7055957/files/Nikatag/Single-Cell-Spatial-Transcriptomics-for-Border-zone-BZ_Cell_Mapping.zip?download=1"
-ZIP_OUTPUT="$DATA_DIR/Single-Cell-Spatial-Transcriptomics-for-Border-zone-BZ_Cell_Mapping_zenodo.zip"
-
-echo "==> Downloading Zenodo ZIP..."
-wget -c "$METADATA_URL" -O "$ZIP_OUTPUT"
-
-echo "==> Unzipping Zenodo ZIP..."
-unzip -o "$ZIP_OUTPUT" -d "$DATA_DIR"
-
-mv "$DATA_DIR"/Nikatag-Single-Cell-Spatial-Transcriptomics-for-Border-zone-*/* "$DATA_DIR"/
 
 echo "==> Flattening filtered_feature_bc_matrix folders..."
 
